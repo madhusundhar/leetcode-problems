c5f0bbe31483f82b54b8b83de043aad81b456d7b
Madhusundharan <madhusundhar28@gmail.com>
2025-12-23 14:02:39 +0530

    Time: 32 ms (99.50%) | Memory: 171.2 MB (84.50%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -1,37 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int maxTwoEvents(int[][] events) {
-        int n = events.length;
-
-        // Sort by start time
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
-
-        // prefixMax[i] = max value from i to end
-        int[] prefixMax = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            prefixMax[i] = Math.max(prefixMax[i + 1], events[i][2]);
+        int n = events.length;
+        int[] f = new int[n + 1];
+        for (int i = n - 1; i >= 0; --i) {
+            f[i] = Math.max(f[i + 1], events[i][2]);
         }
-
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            int currVal = events[i][2];
-
-            // binary search for first event starting after current ends
-            int l = i + 1, r = n;
-            while (l < r) {
-                int mid = (l + r) / 2;
-                if (events[mid][0] > events[i][1]) {
-                    r = mid;
+        for (int[] e : events) {
+            int v = e[2];
+            int left = 0, right = n;
+            while (left < right) {
+                int mid = (left + right) >> 1;
+                if (events[mid][0] > e[1]) {
+                    right = mid;
                 } else {
-                    l = mid + 1;
+                    left = mid + 1;
                 }
             }
-
-            ans = Math.max(ans, currVal + prefixMax[l]);
+            if (left < n) {
+                v += f[left];
+            }
+            ans = Math.max(ans, v);
         }
-
         return ans;
     }
 }
